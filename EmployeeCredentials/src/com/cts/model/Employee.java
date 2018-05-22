3ec01156078e67bbfa6ebb683bb6ec0f26094202
Naarbet1 <reivaxtebraan.rt@gmail.com>
2018-05-22 18:17:03 +0530

    Created employee details

--- EmployeeCredentials/src/com/cts/model/Employee.java
+++ EmployeeCredentials/src/com/cts/model/Employee.java
@@ -1,5 +1,26 @@
 package com.cts.model;
 
-public class Employee {
+import java.util.List;
+public interface Employee {
+
+	public int getsalery=(Integer) null;
+
+	public boolean save(Employee emp);
+
+	public boolean update(Employee emp);
+
+	public boolean delete(int Id);
+
+	public Employee get(int Id);
+
+	public List<Employee> get();
+
+	public void display(Employee emp);
+
+	public void display(List<Employee> empList);
+
+	public int getId();
+
+	public String getName();
 
 }
