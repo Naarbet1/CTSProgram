88a5d4d7e753496e39e76f92f8aa3a11953e4869
abbas886 <abbas886@yahoo.com>
2018-05-24 12:11:29 +0530

    wrong logic

--- OopsBasedPrograms/src/com/oopsbased/program/PrimeOrNot.java
+++ OopsBasedPrograms/src/com/oopsbased/program/PrimeOrNot.java
@@ -1,6 +1,10 @@
 package com.oopsbased.program;
 
+//wrong logic.
+//what about 11, 13 and other prime numbers??
 public class PrimeOrNot {
+	//should return true or false
+	//method name should bi like isPrime
 	public int Prime(int a){
 	if (a % 2 == 0 || a % 3 == 0 || a % 5 == 0 || a % 7 == 0){
 		return a;}else
