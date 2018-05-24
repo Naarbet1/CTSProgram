5ec2d543f85d54ad1239f975ea4806451d8a24c6
abbas886 <abbas886@yahoo.com>
2018-05-24 11:58:17 +0530

    unnecessary variable.

--- OopsBasedPrograms/src/com/oopsbased/program/EvenNumber.java
+++ OopsBasedPrograms/src/com/oopsbased/program/EvenNumber.java
@@ -3,7 +3,7 @@ package com.oopsbased.program;
 public class EvenNumber {
 	public static void main(String[] args) {
 		int A = 2, i;
-
+                //why to take another variable A?
 		for (i = 1; i < 20; i++) {
 			if(A % 2 == 0) {
 				System.out.println(+A);
